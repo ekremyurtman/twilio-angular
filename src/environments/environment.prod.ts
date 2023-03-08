166e949410878a405f3269bf14f982119d07c23b
Ekrem Yurtman <ekrem.yurtman@letme.com>
2023-03-08 16:01:12 +0300

    .node-version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    NODE_VERSION: "14.20.0"
+    /*NODE_VERSION: "14.20.0"*/
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  NODE_VERSION: "14.20.0"
+  /*NODE_VERSION: "14.20.0"*/
 }
 /*
  * In development mode, to ignore zone related error stack frames such as
